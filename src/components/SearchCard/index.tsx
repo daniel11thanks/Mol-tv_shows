16db1f405c8c8d8ed15e04fdfb12897f0cbeb9f2
daniel11thanks <daniel11thanks@gmail.com>
2022-12-14 15:47:12 -0300

    desktop first version

--- src/components/SearchCard/index.tsx
+++ src/components/SearchCard/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { SetStateAction, useState } from "react";
 import { ResultList } from '../../models/result';
+import { DescriptionList } from '../../models/description';
 import Button from '../Button';
 import imgBackup from '../../assets/img/tv.jpg';
 
@@ -14,6 +15,7 @@ function SearchCard() {
     const [resultActive, setResultActive] = useState(false);
     const [description, setDescription] = useState('');
     const [descriptionActive, setdescriptionActive] = useState(false);
+    const [summary, setSummary] = useState('');
 
     const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
         setSearch(event.target.value)
@@ -22,6 +24,7 @@ function SearchCard() {
 
     function handleClick(search: string) {
 
+        setdescriptionActive(false);
         
         axios(`http://api.tvmaze.com/search/shows?q=${search}`)
             .then(response => {
@@ -29,12 +32,22 @@ function SearchCard() {
             })
             .catch(error => console.log('error is', error));
             setResultActive(true);
+            window.scrollTo(0, 5000);
     }
 
-    function handleClickResult(show: object) {
+    function handleClickResult(show: any) {
+        let tagP = show.summary
+        let removeP = tagP.replace(/<\/?[pb]>/g, '')
+        setSummary(removeP)
         setDescription(show);
-        console.log(description);
         setdescriptionActive(true);
+        window.scrollTo(0, 2000);
+    }
+
+    function handleClickReset () {
+        setResultActive(false);
+        setdescriptionActive(false);
+        setResults([])
     }
 
     return (
@@ -43,7 +56,7 @@ function SearchCard() {
                 <h2 className="mol-movies-title">type something and we'll find a series about it</h2>
                 <div className="mol-form-control-container">
                     <input className="mol-form-control" type="text" value={search} onChange={handleChange} />
-                    <Button label='search' operation='reset' click={() => handleClick(search)} />
+                    <Button label='search' operation='search' click={() => handleClick(search)} />
                 </div>
             </div>
             <div className={resultActive ? 'results-card' : ''}  >
@@ -59,18 +72,19 @@ function SearchCard() {
                     })}
                 </div>
             </div>
-            <div className={descriptionActive ? 'description-card'  : ''}>
+            <div className={descriptionActive ? 'description-visible'  : 'description-card-invisible'}>
                 <ul>
-                    <li><strong>Name:</strong> {description.name} </li>
-                    <li><strong>Language:</strong> {description.language} </li>
-                    <li><strong>Genres:</strong> {description.genres} </li>
-                    <li><strong>Premiered:</strong> {description.premiered}</li>
-                    <li><strong>Status:</strong> {description.status} </li>
-                    <li><strong>Summary:</strong> {description.summary}</li>
+                    <li><strong>Name:</strong> {description.name? description.name : 'unavailable' } </li>
+                    <li><strong>Language:</strong> {description.language? description.language : 'unavailable' } </li>
+                    <li><strong>Genres:</strong> {description.genres? description.genres : 'unavailable'} </li>
+                    <li><strong>Premiered:</strong> {description.premiered? description.premiered : 'unavailable'}</li>
+                    <li><strong>Status:</strong> {description.status? description.status : 'unavailable'} </li>
+                    <li><strong>Summary:</strong> {summary? summary : 'unavailable'}</li>
                 </ul>
                 <img src={description.image? description.image.medium : imgBackup} height="280" />
             </div>
-            <div className="description-btn-conteiner">
+            <div className={descriptionActive ? 'description-btn-conteiner-visible' : 'description-btn-conteiner-invisible'}>
+                <Button label='Reset Search' operation='reset' click={() => handleClickReset()}  />
             </div>
         </>
     )
---
+++ src/models/description.ts
@@ -0,0 +1,13 @@
+export type DescriptionList = {
+    score: string;
+    show: {
+        averageRuntime: number;
+        name: string;
+        image: { medium: string, original: string };
+        id: number;
+        premiered: number;
+        status: string;
+        summary: string;
+    };
+    
+};
